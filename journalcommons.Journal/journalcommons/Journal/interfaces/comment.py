e003a4445eb95dc71d79c1e4d8803e3f5b0280f6
Juan Grigera <juan@grigera.com.ar>
2010-04-12 13:12:02 +0100

    Added Comment type

---
+++ journalcommons.Journal/journalcommons/Journal/interfaces/comment.py
@@ -0,0 +1,12 @@
+from zope import schema
+from zope.interface import Interface
+
+from zope.app.container.constraints import contains
+from zope.app.container.constraints import containers
+
+from journalcommons.Journal import JournalMessageFactory as _
+
+class IComment(Interface):
+    """Object containing comments for an article"""
+    
+    # -*- schema definition goes here -*-
