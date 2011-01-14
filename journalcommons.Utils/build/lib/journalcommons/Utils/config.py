d9edc1aeec9852dc24470379e9a5c83faf684036
Juan Grigera <juan@grigera.com.ar>
2011-01-14 16:26:56 -0300

    > Removed old journalcommons.Utils

--- journalcommons.Utils/build/lib/journalcommons/Utils/config.py
+++
@@ -1,9 +0,0 @@
-"""Common configuration constants
-"""
-
-PROJECTNAME = 'journalcommons.Utils'
-
-ADD_PERMISSIONS = {
-    # -*- extra stuff goes here -*-
-    'BookReview': 'journalcommons.Utils: Add BookReview',
-}
--- journalcommons.Utils/build/lib/journalcommons/Utils/interfaces/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- extra stuff goes here -*-
-from bookreview import IBookReview
-
