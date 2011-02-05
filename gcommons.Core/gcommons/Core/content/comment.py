3aa601a4fd6b85b0e5e8f4c472f341c61b50a6c3
Juan Grigera <juan@grigera.com.ar>
2011-02-05 02:31:07 -0300

    + Added permissions to different comment types

--- gcommons.Core/gcommons/Core/content/comment.py
+++ gcommons.Core/gcommons/Core/content/comment.py
@@ -3,6 +3,8 @@
 
 from zope.interface import implements, directlyProvides
 from Acquisition import aq_inner, aq_parent
+from Products.CMFCore import permissions
+from Products.DCWorkflow.utils import modifyRolesForPermission
 
 from Products.Archetypes import atapi
 from Products.ATContentTypes.content import base
@@ -13,6 +15,8 @@ from gcommons.Core import CoreMessageFactory as _
 from gcommons.Core.interfaces import IComment
 from gcommons.Core.config import PROJECTNAME
 
+import logging
+logger = logging.getLogger('gcommons.Core.content.comment')
 
 CommentSchema = schemata.ATContentTypeSchema.copy() + atapi.Schema((
     atapi.ComputedField('title',
@@ -68,6 +72,22 @@ CommentSchema['description'].widget.visible = {'edit' : 'invisible', 'view' : 'i
 schemata.finalizeATCTSchema(CommentSchema, moveDiscussion=False)
 
 
+class CommentType:
+    def __init__(self, id, description, view_roles):
+        self.id = id
+        self.description = description
+        self.view_roles = view_roles
+       
+    def getId(self):
+        return self.id
+        
+    def getTuple(self):
+        return (self.id, self.description, self.id)
+
+    def getViewRoles(self):
+        return self.view_roles
+
+
 class Comment(base.ATCTContent):
     """Object containing comments for an article"""
     implements(IComment)
@@ -78,8 +98,13 @@ class Comment(base.ATCTContent):
     title = atapi.ATFieldProperty('title')
     description = atapi.ATFieldProperty('description')
     
+    types = (
+        CommentType('internal', 'Internal EB Comment', ('EditorialBoard','Owner','Manager',)),
+        CommentType('referee',  'Referee Report', ('EditorialBoard','Owner','Manager',)),
+        CommentType('report',   'Report for the Author', ('EditorialBoard','JournalAuthor','Owner','Manager',)),
+    )
+
     # -*- Your ATSchema to Python Property Bridges Here ... -*-
-    
     def _compute_title(self):
         return "%s by %s" % (self.getCommentType(), ','.join(self.listCreators()))
     
@@ -91,11 +116,32 @@ class Comment(base.ATCTContent):
         """        
         Get the vocabulary of available draft types
         """        
-        vocab = atapi.DisplayList()        
-        vocab.add('internal', 'Internal EB Comment', 'internal')        
-        vocab.add('referee',  'Referee Report', 'referee')        
-        vocab.add('report',   'Report for the Author', 'report')        
-        # TODO: permission.. assigned. 
+        vocab = atapi.DisplayList()
+        for atype in Comment.types:
+            vocab.add(*atype.getTuple())
         return vocab
+        
+    def setCommentType(self, data):
+        field = self.getField('commentType')
+        field.set(self, data)
+        data = field.get(self) # After cleanup
+        
+        # Apply permissions according to type
+        view_roles = None
+        for atype in Comment.types:
+            if data == atype.getId():
+               view_roles = atype.getViewRoles()
+               break
+        if view_roles is None:
+            return
+            
+        for view_perm in (permissions.AccessContentsInformation, permissions.View):
+            modifyRolesForPermission(self, view_perm, view_roles )# if roles is a tuple, this means not to acquire
+        # Only owner can modify this
+        modifyRolesForPermission(self,permissions.ModifyPortalContent,('Owner',))
+
+        
+        
+    
 
 atapi.registerType(Comment, PROJECTNAME)
