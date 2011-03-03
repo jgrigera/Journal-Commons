92042fb44911915b1dd7e64ffd731588033b0b56
Juan Grigera <juan@grigera.com.ar>
2011-03-02 23:57:15 -0300

    + Plone 4: Upgrade folderish classes

---
+++ gcommons.theme.historicalmaterialism/gcommons/theme/historicalmaterialism/browser/interfaces.py
@@ -0,0 +1,9 @@
+from plone.theme.interfaces import IDefaultPloneLayer
+
+
+class IThemeSpecific(IDefaultPloneLayer):
+    """Marker interface that defines a Zope 3 browser layer.
+       If you need to register a viewlet only for the
+       "historicalmaterialism" theme, this interface must be its layer
+       (in historicalmaterialism/viewlets/configure.zcml).
+    """
