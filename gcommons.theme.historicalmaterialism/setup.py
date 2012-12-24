b267d4f125dbf1f39dfa7a13054cd7a18abeedf9
Juan Grigera <juan@grigera.com.ar>
2012-12-24 19:46:38 +0000

    gitignore changes

--- gcommons.theme.historicalmaterialism/setup.py
+++ gcommons.theme.historicalmaterialism/setup.py
@@ -20,7 +20,7 @@ setup(name='gcommons.theme.historicalmaterialism',
       url='http://svn.plone.org/svn/collective/',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['gcommons.theme'],
+      namespace_packages=['gcommons', 'gcommons.theme'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
