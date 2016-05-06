d9b3cdb4917f271167a5eb087259c3e84dac4f80
Juan Grigera <juan@grigera.com.ar>
2016-05-06 18:18:15 +0100

    more bugfixes Plone 5

--- gcommons.Core/gcommons/Core/interfaces/comment.py
+++ gcommons.Core/gcommons/Core/interfaces/comment.py
@@ -1,8 +1,8 @@
 from zope import schema
 from zope.interface import Interface
 
-from zope.app.container.constraints import contains
-from zope.app.container.constraints import containers
+from zope.container.constraints import contains
+from zope.container.constraints import containers
 
 from gcommons.Core import CoreMessageFactory as _
 
